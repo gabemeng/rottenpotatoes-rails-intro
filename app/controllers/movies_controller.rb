11782616fb54192d196b6668e5c0e60791f52785
Gabriel Meng <menggabriel@berkeley.edu>
2020-07-21 03:05:57 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,28 +14,46 @@ class MoviesController < ApplicationController
     #session stays throughout different requests
     #params is for one request only 
     #rails server -b 0.0.0.0
-    
+ 
     @all_ratings = Movie.possibleRatings
-    if params.has_key?(:sortby)
-        @ratings = Hash[@all_ratings.collect {|rating| [rating, 1]}]
-        sortType = params[:sortby]
-        if sortType == 'title'
-            @title_hilite = 'hilite'
-            @movies = Movie.order(:title)
-        else 
-            @release_hilite = 'hilite'
-            @movies = Movie.order(:release_date)
-        end   
-    else
-        if params.has_key?(:ratings)
-            @ratings = params[:ratings]
-            chosenRatings = @ratings.keys
-            @movies = Movie.with_ratings(chosenRatings)
-        else 
-            @ratings = Hash[@all_ratings.collect {|rating| [rating, 1]}]
-            @movies = Movie.all 
-        end 
-    end  
+    if params[:ratings]
+        @ratings = params[:ratings]
+        session[:ratings] = params[:ratings] 
+    else 
+        @ratings = session[:ratings]
+    end 
+      
+    @ratings = Hash[@all_ratings.collect {|rating| [rating, 1]}] unless @ratings  
+    chosenRatings = @ratings.keys
+      
+    if params[:sortby]
+        session[:sortby] = params[:sortby]
+        sortType = session[:sortby]
+    else 
+        sortType = ''
+    end 
+      
+    if sortType == 'title'
+        @title_hilite = 'hilite'
+        @movies = Movie.where(rating: chosenRatings).order(:title)
+    elsif sortType == 'release_date'
+        @release_hilite = 'hilite'
+        @movies = Movie.where(rating: chosenRatings).order(:release_date)
+    else 
+        @movies = Movie.where(rating: chosenRatings)
+    end 
+      
+    if (session[:ratings] != params[:ratings]) || (session[:sortby] != params[:sortby])
+        flash.keep
+        redirect_to movies_path(:sortby => session[:sortby], :ratings => session[:ratings])
+    end
+    
+    
+    #if session[:sortby]
+        #@movies = Movie.where(rating: chosenRatings).order(session[:sortby])
+    #else 
+        #@movies = Movie.where(rating: chosenRatings)
+    #end 
         
   end
 
